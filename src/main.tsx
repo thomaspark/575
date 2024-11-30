4036733ecd14996d253d218d42bd9a7e6047697b
Thomas Park <hello@thomaspark.co>
2024-11-29 21:55:07 -0500

    switch to webview

--- src/main.tsx
+++ src/main.tsx
@@ -1,73 +1,123 @@
-// Learn more at developers.reddit.com/docs
+import './createPost.js';
+
 import { Devvit, useState } from '@devvit/public-api';
-import words from '../data/words.json';
+
+// Defines the messages that are exchanged between Devvit and Web View
+type WebViewMessage =
+  | {
+      type: 'initialData';
+      data: { username: string; currentCounter: number };
+    }
+  | {
+      type: 'setCounter';
+      data: { newCounter: number };
+    }
+  | {
+      type: 'updateCounter';
+      data: { currentCounter: number };
+    };
 
 Devvit.configure({
   redditAPI: true,
+  redis: true,
 });
 
-function shuffle(arr) {
-  return arr.sort(() => 0.5 - Math.random());
-}
-
-let magnets = [];
-magnets.push(...words.always);
-magnets.push(...shuffle(words.adjectives).slice(0, 15));
-magnets.push(...shuffle(words.prepositions).slice(0, 10));
-magnets.push(...shuffle(words.pronouns).slice(0, 10));
-magnets.push(...shuffle(words.verbs).slice(0, 15));
-magnets.push(...shuffle(words.nouns).slice(0, 15));
-magnets.sort();
-
-console.log(magnets);
-
-// Add a menu item to the subreddit menu for instantiating the new experience post
-Devvit.addMenuItem({
-  label: 'Add Magnetry Post',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async (_event, context) => {
-    const { reddit, ui } = context;
-    const subreddit = await reddit.getCurrentSubreddit();
-    const date = new Date().toLocaleDateString('en-CA');
-    await reddit.submitPost({
-      title: `Magnetry ${date}`,
-      subredditName: subreddit.name,
-      // The preview appears while the post loads
-      preview: (
-        <vstack height="100%" width="100%" alignment="middle center">
-          <text size="large">Loading ...</text>
-        </vstack>
-      ),
-    });
-    ui.showToast({ text: 'Created post!' });
-  },
-});
-
-// Add a post type definition
+// Add a custom post type to Devvit
 Devvit.addCustomPostType({
-  name: 'Experience Post',
-  height: 'regular',
-  render: (_context) => {
-    const [counter, setCounter] = useState(0);
+  name: 'Webview Example',
+  height: 'tall',
+  render: (context) => {
+    // Load username with `useAsync` hook
+    const [username] = useState(async () => {
+      const currUser = await context.reddit.getCurrentUser();
+      return currUser?.username ?? 'anon';
+    });
 
+    // Load latest counter from redis with `useAsync` hook
+    const [counter, setCounter] = useState(async () => {
+      const redisCount = await context.redis.get(`counter_${context.postId}`);
+      return Number(redisCount ?? 0);
+    });
+
+    // Create a reactive state for web view visibility
+    const [webviewVisible, setWebviewVisible] = useState(false);
+
+    // When the web view invokes `window.parent.postMessage` this function is called
+    const onMessage = async (msg: WebViewMessage) => {
+      switch (msg.type) {
+        case 'setCounter':
+          await context.redis.set(`counter_${context.postId}`, msg.data.newCounter.toString());
+          context.ui.webView.postMessage('myWebView', {
+            type: 'updateCounter',
+            data: {
+              currentCounter: msg.data.newCounter,
+            },
+          });
+          setCounter(msg.data.newCounter);
+          break;
+        case 'initialData':
+        case 'updateCounter':
+          break;
+
+        default:
+          throw new Error(`Unknown message type: ${msg satisfies never}`);
+      }
+    };
+
+    // When the button is clicked, send initial data to web view and show it
+    const onShowWebviewClick = () => {
+      setWebviewVisible(true);
+      context.ui.webView.postMessage('myWebView', {
+        type: 'initialData',
+        data: {
+          username: username,
+          currentCounter: counter,
+        },
+      });
+    };
+
+    // Render the custom post type
     return (
-      <vstack height="100%" width="100%" gap="medium" alignment="center middle">
-        <image
-          url="logo.png"
-          description="logo"
-          imageHeight={256}
-          imageWidth={256}
-          height="48px"
-          width="48px"
-        />
-        <hstack height="100%" width="100%" gap="medium" alignment="center middle">
-          {magnets.map(magnet => <button key={magnet} name={magnet} size="small">{magnet}</button>)}
-        </hstack>
-        <text size="large">{`Click Counter: ${counter}`}</text>
-        <button appearance="primary" onPress={() => setCounter((counter) => counter + 1)}>
-          Click me!
-        </button>
+      <vstack grow padding="small">
+        <vstack
+          grow={!webviewVisible}
+          height={webviewVisible ? '0%' : '100%'}
+          alignment="middle center"
+        >
+          <text size="xlarge" weight="bold">
+            Example App
+          </text>
+          <spacer />
+          <vstack alignment="start middle">
+            <hstack>
+              <text size="medium">Username:</text>
+              <text size="medium" weight="bold">
+                {' '}
+                {username ?? ''}
+              </text>
+            </hstack>
+            <hstack>
+              <text size="medium">Current counter:</text>
+              <text size="medium" weight="bold">
+                {' '}
+                {counter ?? ''}
+              </text>
+            </hstack>
+          </vstack>
+          <spacer />
+          <button onPress={onShowWebviewClick}>Launch App</button>
+        </vstack>
+        <vstack grow={webviewVisible} height={webviewVisible ? '100%' : '0%'}>
+          <vstack border="thick" borderColor="black" height={webviewVisible ? '100%' : '0%'}>
+            <webview
+              id="myWebView"
+              url="page.html"
+              onMessage={(msg) => onMessage(msg as WebViewMessage)}
+              grow
+              height={webviewVisible ? '100%' : '0%'}
+            />
+          </vstack>
+        </vstack>
       </vstack>
     );
   },
