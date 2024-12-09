49742e25d1cdee5d834ffd7fd02db49d8fd9f4ff
Thomas Park <hello@thomaspark.co>
2024-12-09 12:14:08 -0500

    fix text fallback

--- src/actions/createPost.tsx
+++ src/actions/createPost.tsx
@@ -16,13 +16,14 @@ Devvit.addMenuItem({
     const post = await reddit.submitPost({
       title: `575: ${d}`,
       subredditName: subreddit.name,
-      textFallback: { text: `This post contains content not supported by old Reddit. [Click here to view the full post.](${post})` },
+      textFallback: { text: `This post contains content not supported by old Reddit.` },
       preview: (
         <vstack height="100%" width="100%" alignment="middle center">
           <text size="large">loading...</text>
         </vstack>
       ),
     });
+    await post.setTextFallback({ text: `This post contains content not supported by old Reddit. [Click here to view the full post.](${post})` });
     ui.showToast({ text: 'Created post!' });
     ui.navigateTo(post);
   },
