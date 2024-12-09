08dba57d150bd2199062c48ae2106d1494398bcb
Thomas Park <hello@thomaspark.co>
2024-12-09 15:06:04 -0500

    add daily cron job

--- src/actions/createPost.tsx
+++ src/actions/createPost.tsx
@@ -5,26 +5,51 @@ Devvit.configure({
   redis: true,
 });
 
+const createPost = async function(_event, context) {
+  const { reddit, ui } = context;
+  const subreddit = await reddit.getCurrentSubreddit();
+  const d = new Date().toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'});
+
+  const post = await reddit.submitPost({
+    title: `575: ${d}`,
+    subredditName: subreddit.name,
+    textFallback: { text: `This post contains content not supported by old Reddit.` },
+    preview: (
+      <vstack height="100%" width="100%" alignment="middle center">
+        <text size="large">575</text>
+      </vstack>
+    ),
+  });
+
+  await post.setTextFallback({ text: `This post contains content not supported by old Reddit. [Click here to view the full post.](${post})` });
+  ui.showToast({ text: 'Created post!' });
+  ui.navigateTo(post);
+}
+
 Devvit.addMenuItem({
   label: 'Create New 575 Game',
   location: 'subreddit',
-  onPress: async (_event, context) => {
-    const { reddit, ui } = context;
-    const subreddit = await reddit.getCurrentSubreddit();
-    const d = new Date().toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'});
+  onPress: createPost,
+});
 
-    const post = await reddit.submitPost({
-      title: `575: ${d}`,
-      subredditName: subreddit.name,
-      textFallback: { text: `This post contains content not supported by old Reddit.` },
-      preview: (
-        <vstack height="100%" width="100%" alignment="middle center">
-          <text size="large">575</text>
-        </vstack>
-      ),
-    });
-    await post.setTextFallback({ text: `This post contains content not supported by old Reddit. [Click here to view the full post.](${post})` });
-    ui.showToast({ text: 'Created post!' });
-    ui.navigateTo(post);
+Devvit.addSchedulerJob({
+  name: 'daily_thread',
+  onRun: createPost,
+});
+
+Devvit.addTrigger({
+  event: 'AppInstall',
+  onEvent: async (_, context) => {
+    try {
+      const jobId = await context.scheduler.runJob({
+        cron: '0 0 * * *',
+        name: 'daily_thread',
+        data: {},
+      });
+      await context.redis.set('jobId', jobId);
+    } catch (e) {
+      console.log('error was not able to schedule:', e);
+      throw e;
+    }
   },
 });
