cbdf422ee0d641afbdd2316e955291973b968280
Thomas Park <hello@thomaspark.co>
2024-12-08 21:20:48 -0500

    loading text

--- src/actions/createPost.tsx
+++ src/actions/createPost.tsx
@@ -19,7 +19,7 @@ Devvit.addMenuItem({
       textFallback: { text: `This post contains content not supported by old Reddit. [Click here to view the full post.](${post})` },
       preview: (
         <vstack height="100%" width="100%" alignment="middle center">
-          <text size="large">Loading  ...{context.postId}</text>
+          <text size="large">loading...</text>
         </vstack>
       ),
     });
